edb354edf3cc1843094bf879e549bcfa9083580d
DraedenJensen <draeden.waffles@gmail.com>
2025-03-23 18:41:39 -0600

    dl-type

--- SdnSwitch.py
+++ SdnSwitch.py
@@ -81,7 +81,17 @@ def _handle_PacketIn(event):
       of_msg.actions.append(of.ofp_action_output(port = out_port))
       event.connection.send(of_msg)
       log.info(f"OpenFlow rule set: match traffic from inport {in_port} with destination {packet.payload.protodst}, send to outport {out_port} with destination {reply.protosrc}")
-#     match:
+     
+      # Reverse flow
+      of_msg_r = of.ofp_flow_mod()
+      of_msg_r.match.in_port = out_port
+      of_msg.match.dl_type = 0x800
+      of_msg.match.nw_src = reply.protosrc
+      of_msg.match.nw_dst = packet.payload.protosrc
+      of_msg.actions.append(of.ofp_action_nw_addr.set_src(packet.payload.protodst)) #this should match the real IP address of the selected server
+      of_msg.actions.append(of.ofp_action_output(port = in_port))
+       log.info(f"OpenFlow rule set: match traffic from inport {out_port} with source {reply.protosrc} and destination {packet.payload.protosrc}, send to outport {in_port} with source {packet.payload.protodst}")
+#match:
 # inport=h1-port, dst-ip=10.0.0.10
 # action:
 # set: dst-ip=10.0.0.5
